56c6584e5c147c9ae1cfd4c2e88d577a5b4d9ab2
robyn <65057146+robynsdev@users.noreply.github.com>
2021-02-20 11:53:31 +1100

    added ingredient.model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,12 +11,17 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'Recipe1',
       'So descriptive',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=408%2C370'
+      'https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_1280.jpg'
     ),
     new Recipe(
       'Recipe2',
       'Soso descriptive',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=408%2C370'
+      'https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040_1280.jpg'
+    ),
+    new Recipe(
+      'Recipe3',
+      'Sososo descriptive',
+      'https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_1280.jpg'
     ),
   ];
 
