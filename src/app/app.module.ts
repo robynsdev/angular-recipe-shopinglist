8e416d7bb57821f6fdbd9e49650ea24f3691933d
robyn <65057146+robynsdev@users.noreply.github.com>
2021-03-07 18:49:57 +1100

    added ingredient to recipe service and model and detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     DropdownDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -8,17 +9,20 @@ export class RecipeService {
     new Recipe(
       'Recipe1',
       'So descriptive',
-      'https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_1280.jpg'
+      'https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_1280.jpg',
+      [new Ingredient('Maple Syrup', 1), new Ingredient('Flour', 2)]
     ),
     new Recipe(
       'Recipe2',
       'Soso descriptive',
-      'https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040_1280.jpg'
+      'https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040_1280.jpg',
+      [new Ingredient('Strawberries', 1), new Ingredient('Blueberries', 2)]
     ),
     new Recipe(
       'Recipe3',
       'Sososo descriptive',
-      'https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_1280.jpg'
+      'https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_1280.jpg',
+      [new Ingredient('Sugar', 1), new Ingredient('Salt', 2)]
     ),
   ];
 
